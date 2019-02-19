8048546cc46bfb60267f5b7d5d82a7df7ef415c4
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-02-19 14:22:01 +0300

    Add compat_mkdir and Linux version

--- core/nativeMain/src/kotlinx/files/PosixFileSystem.kt
+++ core/nativeMain/src/kotlinx/files/PosixFileSystem.kt
@@ -53,8 +53,7 @@ class PosixFileSystem : FileSystem {
 
     override fun createDirectory(path: Path): UnixPath {
         checkCompatible(path)
-        // 0x1FF hex == 511 == 0777 oct
-        if (mkdir(path.toString(), 0x1FF) == -1) {
+        if (compat_mkdir(path.toString()) == -1) {
             val errno = errno
             throw IOException(
                 "Failed to create directory $path with error code $errno",
@@ -215,3 +214,4 @@ private fun PosixFileSystem.checkCompatible(path: Path) {
 }
 
 expect fun compat_lstat(path: String, ptr: CPointer<stat>): Int
+expect fun compat_mkdir(path: String): Int
--- core/windowsX64Main/src/kotlinx.files/Compat.kt
+++ core/windowsX64Main/src/kotlinx.files/Compat.kt
@@ -4,3 +4,4 @@ import kotlinx.cinterop.*
 import platform.posix.*
 
 actual fun compat_lstat(path: String, ptr: CPointer<stat>): Int = stat(path, ptr)
+actual fun compat_mkdir(path: String): Int = mkdir(path)
