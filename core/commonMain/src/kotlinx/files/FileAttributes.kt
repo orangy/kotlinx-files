0134e4e6b216efe0d85cf94fc7e69c7ad0b288f6
Ilya Ryzhenkov <orangy@jetbrains.com>
2019-03-20 17:22:08 +0300

    Add documentation to FileAttributes properties

--- core/commonMain/src/kotlinx/files/FileAttributes.kt
+++ core/commonMain/src/kotlinx/files/FileAttributes.kt
@@ -4,12 +4,39 @@ package kotlinx.files
  * Basic attributes associated with a file in a file system
  */
 open class FileAttributes(
+    /**
+     * Indicates if a file system entity is a directory.
+     */
     val isDirectory: Boolean,
+
+    /**
+     * Indicates if a file system entity is a regular file.
+     */
     val isFile: Boolean,
+
+    /**
+     * Indicates if a file system entity is a symbolic link.
+     */
     val isSymbolicLink: Boolean,
+
+    /**
+     * Creation time for the file measured in microseconds since Epoch.
+     */
     val creationTimeUs: Long,
+
+    /**
+     * Last access time for  the file measured in microseconds since Epoch.
+     */
     val lastAccessTimeUs: Long,
+
+    /**
+     * Last modification time for the file measured in microseconds since Epoch.
+     */
     val lastModifiedTimeUs: Long,
+
+    /**
+     * Size of the file in bytes.
+     */
     val size: Long
 )
 
